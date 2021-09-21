9f4dc69fc1751b951cc72a0879ce6291034d6cbd
Aleksandr Zuev <zuev08@gmail.com>
2021-09-21 11:24:41 +0200

    Add events

--- lcdui/main.py
+++ lcdui/main.py
@@ -1,12 +1,26 @@
 from RPLCD.i2c import CharLCD
 
+from event import Event
 from window import Window
+from utils import getch
 
 
 def main():
     lcd = CharLCD('PCF8574', 0x27)
     w = Window()
     lcd.write_string(str(w))
+    while True:
+        c = getch()
+        if c == 'q':
+            break
+        elif c == 'i':
+            w.handle(Event.UP)
+        elif c == 'k':
+            w.handle(Event.DOWN)
+        elif c == 'j':
+            w.handle(Event.LEFT)
+        elif c == 'l':
+            w.handle(Event.RIGHT)
 
 
 if __name__ == "__main__":
--- lcdui/window.py
+++ lcdui/window.py
@@ -1,9 +1,15 @@
 class Window:
     def __init__(self):
+        self.lines = [
+            'Text',
+            'Label:' + '_' * (20 - 6),
+            '<Button> (*)Radio',
+            '|X|CheckBox'
+        ]
+        self.focus = None
+
+    def handle(self, event):
         pass
 
     def __str__(self):
-        return 'Text\r\n' + \
-               'Label:' + '_' * (20 - 6) + '\r\n' + \
-               '<Button> (*)Radio\r\n' + \
-               '|X|CheckBox'
+        return '\r\n'.join(self.lines)
