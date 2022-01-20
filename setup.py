fafb8acf9b9acfb9ae845652dd7f872ebff23fdc
Aleksandr Zuev <zuev08@gmail.com>
2022-01-20 12:39:15 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='lcdui',
+    packages=find_packages('lcdui'),
+    version='0.1.0',
+    description='UI library for 2004 LCD display',
+    author='Aleksandr Zuev',
+    license='MIT',
+)
